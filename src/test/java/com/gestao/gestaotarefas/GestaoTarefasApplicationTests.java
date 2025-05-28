e3b92a2fa197a5e47a8241ff5ae7f172ffd4dc06
flay <souoflay@gmail.com>
2025-05-28 16:01:20 -0300

    feat: primeira versão do projeto Spring

---
+++ src/test/java/com/gestao/gestaotarefas/GestaoTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao.gestaotarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoTarefasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
